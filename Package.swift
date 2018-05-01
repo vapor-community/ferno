70ad397d8d42d68a9a09e7279adbee20c005af91
Austin <astorgaaustin@gmail.com>
2018-05-01 00:38:50 -0400

    fixed Package.json

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FirebaseRealtime",
-            dependencies: ["Vapor"]),
+            dependencies: ["Vapor"],
+            path: "Sources"),
         .testTarget(
             name: "FirebaseRealtimeTests",
             dependencies: ["FirebaseRealtime"]),
